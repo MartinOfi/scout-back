f80ed1b6ca4784b642a5a3f68742c0a80099b587
martinOfi <martin@tqr.la>
2026-03-09 17:28:25 -0300

    refactor(inscripciones): update module imports, remove CajasModule

--- src/modules/inscripciones/inscripciones.module.ts
+++ src/modules/inscripciones/inscripciones.module.ts
@@ -1,18 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Inscripcion } from './entities/inscripcion.entity';
 import { InscripcionesService } from './inscripciones.service';
 import { InscripcionesController } from './inscripciones.controller';
 import { PersonasModule } from '../personas/personas.module';
-import { CajasModule } from '../cajas/cajas.module';
 import { MovimientosModule } from '../movimientos/movimientos.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Inscripcion]),
     PersonasModule,
-    CajasModule,
-    MovimientosModule,
+    forwardRef(() => MovimientosModule),
   ],
   controllers: [InscripcionesController],
   providers: [InscripcionesService],
